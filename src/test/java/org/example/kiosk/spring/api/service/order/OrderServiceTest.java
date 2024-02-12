4869f3fa8326c4a1ebe2d4db41f99916776b8ed9
choi <younggeunn@naver.com>
2024-02-12 20:16:12 +0900

    Business Layer 테스트 (3)

--- src/test/java/org/example/kiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/org/example/kiosk/spring/api/service/order/OrderServiceTest.java
@@ -2,6 +2,8 @@ package org.example.kiosk.spring.api.service.order;
 
 import org.example.kiosk.spring.domain.order.OrderRepository;
 import org.example.kiosk.spring.domain.orderproduct.OrderProductRepository;
+import org.example.kiosk.spring.domain.stock.Stock;
+import org.example.kiosk.spring.domain.stock.StockRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,15 +15,26 @@ import org.example.kiosk.spring.api.service.order.response.OrderResponse;
 import org.example.kiosk.spring.domain.product.Product;
 import org.example.kiosk.spring.domain.product.ProductRepository;
 import org.example.kiosk.spring.domain.product.ProductType;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.example.kiosk.spring.domain.product.ProductSellingStatus.SELLING;
-import static org.example.kiosk.spring.domain.product.ProductType.HANDMADE;
+import static org.example.kiosk.spring.domain.product.ProductType.*;
 
+/*
+    @Transactional 대신 @AfterEach를 사용하여 데이터를 지워준 이유.
+      - 테스트 코드에서 @Transactional을 사용하여 데이터를 지워줄 경우,
+        실제 Service단에서 @Transactional이 누락되어도 정상 동작하는 것처럼 보일 수가 있다.
+        (= Service 단에는 @Transactional을 놓쳐서 잘못된 코드를 작성하더라도, 테스트 코드에서는 정상 동작하므로 실제 릴리즈되는 소스가 오류를 발생하는 코드임을 놓칠 수 있다.)
+      - Ex). createOrderWithNoStock() 테스트의 stock1.deductQuantity(1); 구문이 그 예시.
+             서비스의 @Transactional을 주석처리하고 테스트코드의 @Transactional을 사용하는 경우, 테스트는 통과하지만 서비스로직은 실제 오류를 유발하는 코드가 된다.
+             이 때문에 테스트코드 작성 시 @Transactional을 사용하기보다는 @AfterEach 권장. 또는 @Transactional의 동작원리를 올바르게 인지한 상태로 사용.
+ */
+// @Transactional
 @ActiveProfiles("test")
 @SpringBootTest
 class OrderServiceTest {
@@ -38,11 +51,15 @@ class OrderServiceTest {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private StockRepository stockRepository;
+
     @AfterEach
     void tearDown() {
         orderProductRepository.deleteAllInBatch();
         productRepository.deleteAllInBatch();
         orderRepository.deleteAllInBatch();
+        stockRepository.deleteAllInBatch();
     }
 
     @Test
@@ -105,6 +122,77 @@ class OrderServiceTest {
                         tuple("001", 1000),
                         tuple("001", 1000)
                 );
+    }
+
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 10000);
+        assertThat(orderResponse.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 3000),
+                        tuple("003", 5000)
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
+    }
+
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+    @Test
+    void createOrderWithNoStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stock1.deductQuantity(1); // todo
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
     }
 
     private Product createProduct(ProductType type, String productNumber, int price) {
