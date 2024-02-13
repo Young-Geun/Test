6b6db9c9b6cec0ee3f03a8adf324322bbae9013c
choi <younggeunn@naver.com>
2024-02-13 20:28:23 +0900

    Presentation Layer 테스트 (1)

--- src/main/java/org/example/kiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/org/example/kiosk/spring/api/controller/product/ProductController.java
@@ -1,7 +1,9 @@
 package org.example.kiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
+import org.example.kiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.example.kiosk.spring.api.service.product.ProductService;
 import org.example.kiosk.spring.api.service.product.response.ProductResponse;
@@ -13,6 +15,11 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping("/api/v1/products/new")
+    public void createProduct(ProductCreateRequest request) {
+        productService.createProduct(request);
+    }
 
     @GetMapping("/api/v1/products/selling")
     public List<ProductResponse> getSellingProducts() {
--- src/main/java/org/example/kiosk/spring/api/service/product/ProductService.java
+++ src/main/java/org/example/kiosk/spring/api/service/product/ProductService.java
@@ -1,20 +1,33 @@
 package org.example.kiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
+import org.example.kiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import org.springframework.stereotype.Service;
 import org.example.kiosk.spring.api.service.product.response.ProductResponse;
 import org.example.kiosk.spring.domain.product.Product;
 import org.example.kiosk.spring.domain.product.ProductRepository;
 import org.example.kiosk.spring.domain.product.ProductSellingStatus;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -22,6 +35,18 @@ public class ProductService {
         return products.stream()
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 
 }
