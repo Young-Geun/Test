b473edf884ea17a39f4fe934140e3401aacd2a58
choi <younggeunn@naver.com>
2024-02-15 22:08:58 +0900

    Q. private 메서드의 테스트는 어떻게 하나요?

--- src/main/java/org/example/kiosk/spring/api/service/product/ProductService.java
+++ src/main/java/org/example/kiosk/spring/api/service/product/ProductService.java
@@ -18,10 +18,11 @@ import java.util.stream.Collectors;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductNumberFactory productNumberFactory;
 
     @Transactional
     public ProductResponse createProduct(ProductCreateServiceRequest request) {
-        String nextProductNumber = createNextProductNumber();
+        String nextProductNumber = productNumberFactory.createNextProductNumber();
 
         Product product = request.toEntity(nextProductNumber);
         Product savedProduct = productRepository.save(product);
@@ -37,16 +38,22 @@ public class ProductService {
                 .collect(Collectors.toList());
     }
 
-    private String createNextProductNumber() {
-        String latestProductNumber = productRepository.findLatestProductNumber();
-        if (latestProductNumber == null) {
-            return "001";
-        }
-
-        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
-        int nextProductNumberInt = latestProductNumberInt + 1;
-
-        return String.format("%03d", nextProductNumberInt);
-    }
+    /**
+        - private 메서드의 테스트
+          : private 메서드는 테스트가 불필요하다.
+            그 이유는 클라이언트 기준에서는 공개된 public 메서드만 알고 있기 때문에 private 메서드까지의 검증은 불필요하다.
+            그럼에도 불구하고 테스트가 필요한 경우는 객체를 분리시킬 시점이라는 반증이기도 하다.
+     */
+//    private String createNextProductNumber() {
+//        String latestProductNumber = productRepository.findLatestProductNumber();
+//        if (latestProductNumber == null) {
+//            return "001";
+//        }
+//
+//        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+//        int nextProductNumberInt = latestProductNumberInt + 1;
+//
+//        return String.format("%03d", nextProductNumberInt);
+//    }
 
 }
